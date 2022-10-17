8068efc8a9d8ff395aa70c17e36e853f19e25458
tanukesensei <luanvs.hack@gmail.com>
2022-10-17 12:00:11 -0300

    Updates

---
+++ _nuxt/static/1666018520/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666018520",layout:"default",error:a,state:{isDark:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0395817b"}}}}(null,"\u002F"));
